d37fa2c60408b58321cab0762af8cbe07691d6e4
Shafiul Azam <ishafiul@gmail.com>
2012-04-10 14:17:29 +0600

    Documentation for 1.3.0b

---
+++ navtree.js
@@ -0,0 +1,366 @@
+var NAVTREE =
+[
+  [ "Swadhin", "index.html", [
+    [ "Data Structures", "annotated.html", [
+      [ "Blocks", "class_blocks.html", null ],
+      [ "Config", "class_config.html", null ],
+      [ "Core", "class_core.html", null ],
+      [ "CoreController", "class_core_controller.html", null ],
+      [ "CoreException", "class_core_exception.html", null ],
+      [ "CoreForm", "class_core_form.html", null ],
+      [ "CoreModel", "class_core_model.html", null ],
+      [ "CoreValidator", "class_core_validator.html", null ],
+      [ "CoreView", "class_core_view.html", null ],
+      [ "DbException", "class_db_exception.html", null ],
+      [ "DbGeneric", "class_db_generic.html", null ],
+      [ "DbMysql", "class_db_mysql.html", null ],
+      [ "DbMysql_query_builder", "class_db_mysql__query__builder.html", null ],
+      [ "DbPdo", "class_db_pdo.html", null ],
+      [ "DbPdo_mysql", "class_db_pdo__mysql.html", null ],
+      [ "Error", "class_error.html", null ],
+      [ "Funcs", "class_funcs.html", null ],
+      [ "Html", "class_html.html", null ],
+      [ "LibAuthentication", "class_lib_authentication.html", null ],
+      [ "LibFiles", "class_lib_files.html", null ],
+      [ "MaliciousClass", "class_malicious_class.html", null ],
+      [ "PHPizza", "class_p_h_pizza.html", null ],
+      [ "Template", "class_template.html", null ],
+      [ "Validator", "class_validator.html", null ]
+    ] ],
+    [ "Data Structure Index", "classes.html", null ],
+    [ "Class Hierarchy", "hierarchy.html", [
+      [ "Config", "class_config.html", null ],
+      [ "Core", "class_core.html", null ],
+      [ "CoreController", "class_core_controller.html", null ],
+      [ "CoreException", "class_core_exception.html", null ],
+      [ "CoreForm", "class_core_form.html", null ],
+      [ "CoreModel", "class_core_model.html", null ],
+      [ "CoreValidator", "class_core_validator.html", [
+        [ "Validator", "class_validator.html", null ]
+      ] ],
+      [ "CoreView", "class_core_view.html", [
+        [ "Template", "class_template.html", null ],
+        [ "Template", "class_template.html", null ]
+      ] ],
+      [ "DbException", "class_db_exception.html", null ],
+      [ "DbGeneric", "class_db_generic.html", [
+        [ "DbMysql_query_builder", "class_db_mysql__query__builder.html", [
+          [ "DbMysql", "class_db_mysql.html", null ],
+          [ "DbPdo_mysql", "class_db_pdo__mysql.html", null ]
+        ] ],
+        [ "DbPdo", "class_db_pdo.html", null ]
+      ] ],
+      [ "Error", "class_error.html", null ],
+      [ "Funcs", "class_funcs.html", null ],
+      [ "Html", "class_html.html", [
+        [ "Blocks", "class_blocks.html", null ]
+      ] ],
+      [ "LibAuthentication", "class_lib_authentication.html", null ],
+      [ "LibFiles", "class_lib_files.html", null ],
+      [ "MaliciousClass", "class_malicious_class.html", null ],
+      [ "PHPizza", "class_p_h_pizza.html", null ]
+    ] ],
+    [ "Data Fields", "functions.html", null ],
+    [ "File List", "files.html", [
+      [ "/media/sobkichu/xampp/www/phpizza/config.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/index.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/phpinfo.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/post_script.php", "post__script_8php.html", null ],
+      [ "/media/sobkichu/xampp/www/phpizza/pre_script.php", "pre__script_8php.html", null ],
+      [ "/media/sobkichu/xampp/www/phpizza/userconfig.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/core/class/blocks.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/core/class/core.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/core/class/error.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/core/class/funcs.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/core/class/html.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/core/class/validator.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/core/class/core/controller.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/core/class/core/exception.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/core/class/core/form.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/core/class/core/model.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/core/class/core/validator.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/core/class/core/view.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/core/class/db/exception.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/core/class/db/generic.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/core/class/db/mysql.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/core/class/db/mysql_query_builder.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/core/class/db/pdo.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/core/class/db/pdo_mysql.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/core/class/lib/authentication.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/core/class/lib/files.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/core/funcs/form.php", "funcs_2form_8php.html", null ],
+      [ "/media/sobkichu/xampp/www/phpizza/core/funcs/general.php", "general_8php.html", null ],
+      [ "/media/sobkichu/xampp/www/phpizza/custom/class/malicious.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/templates/plainclean/index.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/templates/plainclean/template.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/templates/WhiteLove/index.php", null, null ],
+      [ "/media/sobkichu/xampp/www/phpizza/templates/WhiteLove/template.php", null, null ]
+    ] ],
+    [ "Directories", "dirs.html", [
+      [ "phpizza", "dir_b99a9de45bdaa3c1e202f63676334944.html", [
+        [ "core", "dir_74c96714572a89d9ae7f5ff589ce1ee5.html", [
+          [ "class", "dir_08b5b22413e24777a9518a8a04d7c146.html", [
+            [ "core", "dir_8dcbeca0abb5d7f99f594d152100fb34.html", null ],
+            [ "db", "dir_492d96ba6ed54c07d4b152fb9924b56c.html", null ],
+            [ "lib", "dir_5843821a2e63e095482ed0fa8a108e17.html", null ]
+          ] ],
+          [ "funcs", "dir_241e319f6572a50f4ca06ffe2b991288.html", null ]
+        ] ],
+        [ "custom", "dir_9a88fed9cda65d5fbaf06c1f5bc685e8.html", [
+          [ "class", "dir_9cc7ad0c4ecb5dbeba1eb5b8073c6194.html", null ]
+        ] ],
+        [ "templates", "dir_567f129957e236e79f25544e0b88bf1d.html", [
+          [ "plainclean", "dir_ea4018750ea1ee03110f7125ddcf9f00.html", null ],
+          [ "WhiteLove", "dir_d30e5b697e6bc404a488b73da96fa2a0.html", null ]
+        ] ]
+      ] ]
+    ] ],
+    [ "Globals", "globals.html", null ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
